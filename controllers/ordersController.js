3148b40cd27a7c3495220823eb4c5bd8bdf7902a
Ludovic Saule <ludovic.saule@dci.education>
2023-03-30 14:03:46 +0200

    url updated

--- controllers/ordersController.js
+++ controllers/ordersController.js
@@ -39,8 +39,8 @@ export const openStripeChekoutPage = async (req, res) => {
 		const session = await stripe.checkout.sessions.create({
 			line_items,
 			mode: 'payment',
-			success_url: `http://localhost:5173/cart?success=true`,
-			cancel_url: `http://localhost:5173/cart?success=false`,
+			success_url: `https://e-commerce-dci.onrender.com/#/cart?success=true`,
+			cancel_url: `https://e-commerce-dci.onrender.com/#/cart?success=false`,
 		});
 
 		res.json({ success: true, url: session.url });
